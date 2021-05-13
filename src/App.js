6ce16216886bda311d7ad0e148b71bc3c9228b80
Anna Ira <annairahurnaus@mytum.de>
2021-05-13 17:51:25 +0200

    use checkin data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
-import categories from './categories.json';
-import products from './products.json';
+import factories from './factories.json';
+import data from './data.json';
 import {process} from '@progress/kendo-data-query';
 import {Grid, GridColumn} from '@progress/kendo-react-grid';
 import {DropDownList} from '@progress/kendo-react-dropdowns';
@@ -11,52 +11,52 @@ import {Window} from '@progress/kendo-react-dialogs';
 class App extends Component {
 
     state = {
-        dropdownlistCategory: null,
+        dropdownlistFactory: null,
         gridDataState: {
             sort: [
-                {field: "ProductName", dir: "asc"}
+                {field: "year", dir: "asc"}
             ],
             skip: 0,
             take: 10
         },
         windowVisible: false,
         gridClickedRow: {}
-    }
+    };
 
     handleDropDownChange = (e) => {
-        let newDataState = {...this.state.gridDataState}
-        if (e.target.value.CategoryID !== null) {
+        let newDataState = {...this.state.gridDataState};
+        if (e.target.value.factoryID !== null) {
             newDataState.filter = {
                 logic: 'and',
-                filters: [{field: 'CategoryID', operator: 'eq', value: e.target.value.CategoryID}]
-            }
+                filters: [{field: 'factoryID', operator: 'eq', value: e.target.value.factoryID}]
+            };
             newDataState.skip = 0
         } else {
-            newDataState.filter = []
+            newDataState.filter = [];
             newDataState.skip = 0
         }
         this.setState({
-            dropdownlistCategory: e.target.value.CategoryID,
+            dropdownlistFactory: e.target.value.factoryID,
             gridDataState: newDataState
         });
-    }
+    };
 
     handleGridDataStateChange = (e) => {
         this.setState({gridDataState: e.dataState});
-    }
+    };
 
     handleGridRowClick = (e) => {
         this.setState({
             windowVisible: true,
             gridClickedRow: e.dataItem
         });
-    }
+    };
 
     closeWindow = (e) => {
         this.setState({
             windowVisible: false
         });
-    }
+    };
 
     render() {
         return (
@@ -64,40 +64,45 @@ class App extends Component {
                 <h1>Hello KendoReact!</h1>
                 <p>
                     <DropDownList
-                        data={categories}
-                        dataItemKey="CategoryID"
-                        textField="CategoryName"
-                        defaultItem={{CategoryID: null, CategoryName: "Product categories"}}
+                        data={factories}
+                        dataItemKey="factoryID"
+                        textField="factoryName"
+                        defaultItem={{factoryID: null, factoryName: "Factories"}}
                         onChange={this.handleDropDownChange}
                     />
-                    &nbsp; Selected category ID: <strong>{this.state.dropdownlistCategory}</strong>
+                    &nbsp; Selected factory ID: <strong>{this.state.dropdownlistFactory}</strong>
                 </p>
 
                 <Grid
-                    data={process(products, this.state.gridDataState)}
+                    data={process(data, this.state.gridDataState)}
                     pageable={true}
                     sortable={true}
                     onRowClick={this.handleGridRowClick}
                     {...this.state.gridDataState}
                     onDataStateChange={this.handleGridDataStateChange}
                     style={{height: "400px"}}>
-                    <GridColumn field="ProductName" title="Product Name"/>
-                    <GridColumn field="UnitPrice" title="Price" format="{0:c}"/>
-                    <GridColumn field="UnitsInStock" title="Units in Stock"/>
-                    <GridColumn field="Discontinued" cell={checkboxColumn}/>
+                    <GridColumn field="factoryID" title="Factory ID"/>
+                    <GridColumn field="factoryName" title="Factory Name"/>
+                    <GridColumn field="year" title="Hired in (year)"/>
+                    <GridColumn field="paid" title="Full Payment on time"/>
+                    <GridColumn field="overtime" title="Overtime (hours)"/>
+                    <GridColumn field="unpaid_overtime" title="Unpaid Overtime (hours)"/>
+                    <GridColumn field="age" title="age"/>
+                    <GridColumn field="gender" title="gender"/>
+                    <GridColumn field="safety" title="safety"/>
                 </Grid>
                 {this.state.windowVisible &&
                 <Window
-                    title="Product Details"
+                    title="Check-In Data"
                     onClose={this.closeWindow}
                     height={250}>
                     <dl style={{textAlign: "left"}}>
                         <dt>Product Name</dt>
-                        <dd>{this.state.gridClickedRow.ProductName}</dd>
+                        <dd>{this.state.gridClickedRow.factory}</dd>
                         <dt>Product ID</dt>
-                        <dd>{this.state.gridClickedRow.ProductID}</dd>
+                        <dd>{this.state.gridClickedRow.year}</dd>
                         <dt>Quantity per Unit</dt>
-                        <dd>{this.state.gridClickedRow.QuantityPerUnit}</dd>
+                        <dd>{this.state.gridClickedRow.paid}</dd>
                     </dl>
                 </Window>
                 }
