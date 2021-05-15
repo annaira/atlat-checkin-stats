e6489ff8663e5870512885fa52b34da5dbfaa256
Anna Ira <annairahurnaus@mytum.de>
2021-05-15 09:22:42 +0200

    add our cute font and colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
-
-
 import './App.scss';
 import MyAppBar from "./components/MyAppBar";
-
 import Tiles from "./components/TileLayout/Tiles"
-
 
 class App extends Component {
 
